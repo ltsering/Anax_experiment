4e1d09337a65f8b525f055be0fa28eb03385fe24
ltsering <lotsering2003@gmail.com>
2021-02-17 22:28:43 +0000

    preparing master script for anax experiment

--- Anax_script.R
+++ Anax_script.R
@@ -5,6 +5,7 @@ list()
 install.packages('Rcmdr')
 install.packages('car')
 install.packages('psych')
+install.packages('rlang')
 #libraries for the Anax experiment
 library(ggplot2)
 library(tidyr)
@@ -13,8 +14,11 @@ library(Rcmdr)
 library(car)
 library(psych)
 library(reshape2)
+library(rlang)
+
 # master file imported from dropbox manually via the import xl option. 
 
+read_excel()
 head(master_file)
 tail(master_file)
 
@@ -22,11 +26,15 @@ tail(master_file)
 
 master_data <- master_file[c(1:80), c(1:17)]
 master_data$Temperature <- as.factor(master_data$Temperature)
+# Initialdry_gm to numeric 
+options(scipen=999)
+
+master_data
 
 master_data$Initialdry_gm <- as.integer(master_data$Initialdry_gm)
 
 summary(master_data)
 str(master_data)
-options(scipen=999)
+
 
 
